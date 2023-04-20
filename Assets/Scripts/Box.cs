8dc87da63a7f00fe0ed42c98775c4a1a6cbf075e
DrMax <tehnolog3ooo@yandex.ru>
2023-04-20 10:14:11 +0300

    RemoverAutoDestroyBox

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -5,27 +5,10 @@ using UnityEngine.Events;
 
 public class Box : MonoBehaviour
 {
-    [SerializeField] private float _delayDestroy;
     [SerializeField] private ParticleSystem _spawnEffect;
 
     private void Start()
     {
-        StartCoroutine(DelayOpen());
         Instantiate(_spawnEffect, transform.position, Quaternion.identity, null);
-    }
-
-    private void OnDisable()
-    {
-        StopCoroutine(DelayOpen());
-    }
-
-    private IEnumerator DelayOpen()
-    {
-        float delaySeconds = _delayDestroy;
-        var waitForDelaySeconds = new WaitForSeconds(delaySeconds);
-
-        yield return waitForDelaySeconds;
-
-        Destroy(gameObject);
     }
 }
